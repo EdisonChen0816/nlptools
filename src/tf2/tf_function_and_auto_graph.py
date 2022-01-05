763f1eb72cb922abd50b4f4bc4efa080c1e25789
chenhao140 <chenhao140@meituan.com>
2022-01-05 14:41:40 +0800

    tf.function and autograph

---
+++ src/tf2/tf_function_and_auto_graph.py
@@ -0,0 +1,66 @@
+# encoding=utf-8
+from tensorflow import keras
+import tensorflow as tf
+from sklearn.datasets import fetch_california_housing
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+print(tf.__version__)
+
+
+# tf.function and autograph
+def scaled_elu(z, scale=1.0, alpha=1.0):
+    # z >= 0 ? scale * z : scale * alpha * tf.nn.elu(z)
+    is_positive = tf.greater_equal(z, 0.0)
+    return scale * tf.where(is_positive, z, alpha * tf.nn.elu(z))
+
+
+print(scaled_elu(tf.constant(-3.)))
+print(scaled_elu(tf.constant([-3., -2.5])))
+
+
+scaled_elu_tf = tf.function(scaled_elu)
+print(scaled_elu_tf(tf.constant(-3.)))
+print(scaled_elu_tf(tf.constant([-3., -2.5])))
+
+print(scaled_elu_tf.python_function is scaled_elu)
+
+
+# 1 + 1/2 + 1/2^2 + ... + 1/2^n
+@tf.function
+def converge_to_2(n_iters):
+    total = tf.constant(0.)
+    increment = tf.constant(1.)
+    for _ in range(n_iters):
+        total += increment
+        increment /= 2.0
+    return total
+
+
+print(converge_to_2(20))
+
+
+def display_tf_code(func):
+    code = tf.autograph.to_code(func)
+    return code
+
+
+print(display_tf_code(scaled_elu))
+
+
+var = tf.Variable(0.)
+@tf.function
+def add_21():
+    return var.assign_add(21)
+
+
+print(add_21())
+
+
+@tf.function
+def cube(z):
+    return tf.pow(z, 3)
+
+
+print(cube(tf.constant([1., 2., 3.])))
+print(cube(tf.constant([1, 2, 3])))
