0c16b87af7cd4ad5eab497d202308ace17e1406a
chenhao140 <chenhao140@meituan.com>
2022-01-24 20:45:06 +0800

    tf求导

--- src/tf2/tf_diffs.py
+++ src/tf2/tf_diffs.py
@@ -30,3 +30,88 @@ def approximate_gradient(g, x1, x2, eps=1e-3):
 
 
 print(approximate_gradient(g, 2., 3.))
+
+
+x1 = tf.Variable(2.0)
+x2 = tf.Variable(3.0)
+with tf.GradientTape() as tape:
+    z = g(x1, x2)
+
+dz_x1 = tape.gradient(z, x1)
+print(dz_x1)
+
+try:
+    dz_x2 = tape.gradient(z, x2)
+except RuntimeError as ex:
+    print(ex)
+
+
+x1 = tf.Variable(2.0)
+x2 = tf.Variable(3.0)
+with tf.GradientTape(persistent=True) as tape:
+    z = g(x1, x2)
+
+dz_x1 = tape.gradient(z, x1)
+dz_x2 = tape.gradient(z, x2)
+print(dz_x1, dz_x2)
+del tape
+
+
+x1 = tf.Variable(2.0)
+x2 = tf.Variable(3.0)
+with tf.GradientTape() as tape:
+    z = g(x1, x2)
+
+dz_x1x2 = tape.gradient(z, [x1, x2])
+print(dz_x1x2)
+
+
+x1 = tf.constant(2.0)
+x2 = tf.constant(3.0)
+with tf.GradientTape() as tape:
+    tape.watch(x1)
+    tape.watch(x2)
+    z = g(x1, x2)
+
+dz_x1x2 = tape.gradient(z, [x1, x2])
+print(dz_x1x2)
+
+
+x = tf.Variable(5.0)
+with tf.GradientTape() as tape:
+    z1 = 3 * x
+    z2 = x ** 2
+print(tape.gradient([z1, z2], x))
+
+
+x1 = tf.Variable(2.0)
+x2 = tf.Variable(3.0)
+with tf.GradientTape(persistent=True) as outer_tape:
+    with tf.GradientTape(persistent=True) as inner_tape:
+        z = g(x1, x2)
+    inner_grads = inner_tape.gradient(z, [x1, x2])
+outer_grads = [outer_tape.gradient(inner_grad, [x1, x2]) for inner_grad in inner_grads]
+print(outer_grads)
+del inner_tape
+del outer_tape
+
+
+learning_rate = 0.1
+x = tf.Variable(0.0)
+for _ in range(100):
+    with tf.GradientTape() as tape:
+        z = f(x)
+    dz_dx = tape.gradient(z, x)
+    x.assign_sub(learning_rate * dz_dx)
+print(x)
+
+
+learning_rate = 0.1
+x = tf.Variable(0.0)
+optimizer = keras.optimizers.SGD(lr=learning_rate)
+for _ in range(100):
+    with tf.GradientTape() as tape:
+        z = f(x)
+    dz_dx = tape.gradient(z, x)
+    optimizer.apply_gradients([(dz_dx, x)])
+print(x)
