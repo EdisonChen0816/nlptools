73659ca852e768872ef1e4d74b6819d660a67e96
chenhao0816 <edison.chen@mthreads.com>
2022-11-17 12:43:19 +0800

    网络爬虫

---
+++ src/nlp_data_collection/README.md
@@ -0,0 +1,2 @@
+# NLP_data_collection
+Some data crawer &amp; clean &amp; collect tools for NLP
---
+++ src/nlp_data_collection/sim_baidu/leek_config.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+此文件leek_config.py是自动生成的leek框架的数据库配置文件。
+"""
+# redis连接配置
+#redis_host = '127.0.0.1'
+#redis_password = ''
+#redis_port = 6379
+#redis_db = 0
+#redis_ssl = False
+
+# kafka配置连接信息
+#kafka_port = 9092
+#kafka_host = '127.0.0.1'
+#kafka_username = ''
+#kafka_password = ''
